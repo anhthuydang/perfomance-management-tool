4b69f4fb0235d220f78f52678d2897e70b26894a
Anh Dang <atd30392@gmail.com>
2020-08-03 12:29:17 -0400

    refactored changeBackgroundColor funtion for buttons

--- src/components/areaChartText/AreaChartText.js
+++ src/components/areaChartText/AreaChartText.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import './AreaChartText.css';
 
 class AreaChartText extends Component {
-
+   
     changeColor = (e) => {
-        // //remove background color of the previously clicked button
+        //remove background color of the previously clicked button
         const clickedButton = document.querySelector(".clickedButton");
         if (clickedButton !== null) {
             clickedButton.classList.remove("clickedButton");
         } 
-        // //change background color of the clicked button
-        const newButton = document.getElementById(e.target.id);
+        //change background color of the clicked button
+        const newButton = e.currentTarget;
         newButton.classList.add("clickedButton");
     }
 
@@ -20,22 +20,22 @@ class AreaChartText extends Component {
                 <h3>{this.props.chosenObject} Trend</h3>
                 <ul>
                 <li>
-                    <button className="time" id="day" onClick={this.changeColor}>Day</button>
+                    <button onClick={this.changeColor}>Day</button>
                 </li>
                 <li>
-                    <button className="time" id="week" onClick={this.changeColor}>Week</button>
+                    <button onClick={this.changeColor}>Week</button>
                 </li>
                 <li>
-                    <button className="time" id="month" onClick={this.changeColor}>Month</button>
+                    <button onClick={this.changeColor}>Month</button>
                 </li>
                 <li>
-                    <button className="time" id="quarter" onClick={this.changeColor}>Quarter</button>
+                    <button onClick={this.changeColor}>Quarter</button>
                 </li>
                 <li>
-                    <button className="time" id="half" onClick={this.changeColor}>Half</button>
+                    <button onClick={this.changeColor}>Half</button>
                 </li>
                 <li>
-                    <button className="time" id="year" onClick={this.changeColor}>Year</button>
+                    <button onClick={this.changeColor}>Year</button>
                 </li>
                 </ul>
             </div>
